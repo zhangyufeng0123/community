83b052e3961025111452701bd75e21e51823e1a2
zhangyufeng0123 <1691903105@qq.com>
2023-10-08 15:12:19 +0800

    删除了hello.html文件，利用bootstrap新增了网页的头

--- src/main/java/life/majiang/community/controller/HelloController.java
+++ src/main/java/life/majiang/community/controller/HelloController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
     @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name")String name, Model model) {
-        model.addAttribute("name", name);
-        return "hello";
+    public String index() {
+        return "index";
     }
 }
