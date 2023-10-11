c4b74dff080fadbdbad98267698469e7b878fb06
zhangyufeng0123 <1691903105@qq.com>
2023-10-11 11:00:45 +0800

    Revert "对前端页面进行修改，动态显示用户和登录选项"
    This reverts commit 8e37742e109cfc9b5dc4bff0bce730419a317d7b.

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -4,49 +4,27 @@ import life.majiang.community.dto.AccessTokenDTO;
 import life.majiang.community.dto.GithubUser;
 import life.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
-
-    @Value("${github.client.id}")
-    private String clientId;
-
-    @Value("${github.client.secret}")
-    private String clientSecret;
-
-    @Value("${github.redirect.uri}")
-    private String redirectUri;
-
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state,
-                           HttpServletRequest request) {
+                           @RequestParam(name="state") String state) {
         AccessTokenDTO dto = new AccessTokenDTO();
         dto.setCode(code);
         dto.setState(state);
-        dto.setClient_id(clientId);
-        dto.setClient_secret(clientSecret);
-        dto.setRedirect_url(redirectUri);
+        dto.setClient_id("cde0c98297689f1858b7");
+        dto.setClient_secret("2c472ec5dac90b8eea621bc8e2fb9a92d7002750");
+        dto.setRedirect_url("http://localhost:8887/callback");
         String accessToken = githubProvider.getAccessToken(dto);
         GithubUser user = githubProvider.getUser(accessToken);
-
-        if (user != null){
-            // 登录成功，写cookie 和 session
-            request.getSession().setAttribute("user", user);
-            return "redirect:/";
-        } else {
-            // 登录失败，重新登录
-            return "redirect:/";
-        }
+        System.out.println(user.getName());
+        return "index";
     }
 }
